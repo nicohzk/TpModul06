e658dd233cddc649d74c3580278a47c5f6bad6b1
nicohzk <nicohzk05@gmail.com>
2025-03-22 10:47:30 +0700

    implementasi design by contract

--- TpModul06_103022300127/Program.cs
+++ TpModul06_103022300127/Program.cs
@@ -1,31 +1,79 @@
-﻿class SayaTubeVideo {
+﻿using System.Diagnostics.Contracts;
+class SayaTubeVideo {
     private int id;
     private String title;
     private int PlayCount;
 
     public SayaTubeVideo(String judul)
     {
-        this.title = judul;
         Random rand = new Random();
+
+            
+        Contract.Requires(judul != null);
+        Contract.Requires(judul.Length <= 100);
+
+            
+        if (judul == null)
+        {    
+            throw new Exception("title tidak boleh Null");
+        }
+            
+        if (judul.Length >= 100) {
+            throw new Exception("Judul tidak boleh lebih dari 100 char");
+            
+        }
+        this.title = judul;
         this.id = rand.Next(10000,99999);
         this.PlayCount = 0;
     }
 
-    public void IncreasePlayCount(int play) {
-        this.PlayCount += play;
+        
+    public void IncreasePlayCount(int play) {            
+        try            
+        {       
+            Contract.Requires(play <= 10000000, "Input penambahan play count maksimal 10.000.000 per panggilan method-nya.");            
+            checked            
+            {            
+                this.PlayCount += play;                
+            }        
+        }
+            
+        catch (OverflowException e) {        
+            Console.WriteLine("Error: Play count melebihi batas maksimum integer.");  
+        }
+        
     }
 
+    
     public void PrintVideoDetails() {
         Console.WriteLine("id = " + this.id);
         Console.WriteLine("title = " + this.title);
         Console.WriteLine("playCount = " + this.PlayCount);
+        
     }
 }
-
 public class system {
-    public static void Main() {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Hizkia Nicander Budiyanto");
-        video.PrintVideoDetails();
-    }
+    public static void Main()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            string judul;
+            Console.Write("Masukkan Judul: ");
+            judul = Console.ReadLine();
+            try
+            {
 
+                SayaTubeVideo video = new SayaTubeVideo(judul);
+                video.PrintVideoDetails();
+                video.IncreasePlayCount(1000000000);
+                video.IncreasePlayCount(2000000000);
+                video.PrintVideoDetails();
+            }
+            catch (Exception e)
+            { 
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
 }
